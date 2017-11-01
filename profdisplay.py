427e54124f96923826856791de4313fc2e63ce39
DocenkoG <docn@mail.ru>
2017-11-01 18:37:01 +0300

    initial commit

---
+++ profdisplay.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+import os
+import os.path
+import logging
+import logging.config
+#import io
+import sys
+#import configparser
+#import time
+import profdisplay_converter
+import shutil
+
+global log
+global myname
+
+
+
+def make_loger():
+    global log
+    logging.config.fileConfig('logging.cfg')
+    log = logging.getLogger('logFile')
+
+
+
+def main( ):
+    global  myname
+    global  mydir
+   
+    make_loger()
+    log.info('------------  '+myname +'  ------------')
+
+#    if  specvideoproect_downloader.download( myname ) :
+    profdisplay_converter.convert2csv( myname )
+#   shutil.copy2( myname + '.csv', 'c://AV_PROM/prices/' + myname +'/'+ myname + '.csv')
+    shutil.copy2( 'python.log',    'c://AV_PROM/prices/' + myname +'/python.log')
+
+if __name__ == '__main__':
+    global  myname
+    global  mydir
+    myname   = os.path.basename(os.path.splitext(sys.argv[0])[0])
+    mydir    = os.path.dirname (sys.argv[0])
+    if ('' != mydir) : os.chdir(mydir)
+    main( )
