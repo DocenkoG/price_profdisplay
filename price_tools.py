2f280198b9681e7a33a58d04c9c52d6f051ee7e0
DocenkoG <docn@mail.ru>
2024-05-06 13:50:03 +0300

    Полностью переделано по образце спецвидеопроекта. Тест пройден успешно.

--- price_tools.py
+++ price_tools.py
@@ -4,6 +4,36 @@ import openpyxl                     # Для .xlsx
 import re
 
 
+
+def openX(fileName ):
+    typeX = fileName[fileName.find('.')+1 :]
+    if typeX.lower() == 'xlsx':
+        book = openpyxl.load_workbook(filename = fileName, read_only=False, keep_vba=False, data_only=False) # xlsx
+    else:
+        book = xlrd.open_workbook( fileName.encode('cp1251'), formatting_info=True)                          # xls
+    return book
+
+
+
+def sheetByName( fileName
+                ,sheetName):
+    typeX = fileName[fileName.find('.')+1 :]
+    try:
+        if typeX.lower() == 'xls':
+            book = xlrd.open_workbook( fileName.encode('cp1251'), formatting_info=True)                          # xls
+            sheet = book.sheet_by_name(sheetName)
+        else:
+            book = openpyxl.load_workbook(filename=fileName, read_only=False, keep_vba=False, data_only=False)  # xlsx
+            sheet = book[sheetName]  # xlsx
+    except Exception as e:
+        print(e)
+        sheet = False
+        book = False
+    return book, sheet
+
+    #sheet = book.worksheets[0]                                                                              # xlsx
+    #sheet = book.sheets()[0]                                                                                # xls
+
 def getCellXlsx(  row       # номер строки
                 , col       # номер колонки 
                 , isDigit   # Признак, числовое ли значение нужно из этого поля
@@ -26,17 +56,19 @@ def getCellXlsx(  row       # номер строки
             else :
                 ss = str(cellValue)
         else :
-            ss = '0'
+#           ss = '0'
+            try:
+                ss = str(float(cellValue.replace('руб.','').replace('р','').replace(',','.').replace(' ','')))
+            except ValueError as e:
+                ss='0'
     else :
         if (cellValue == None) : 
             ss = ''
         elif (cellType in ('n')) :                    # numeric
-            '''
             if int(cellValue) == cellValue:
                 ss = str(int(cellValue))
             else :
-            '''
-            ss = str(cellValue)
+                ss = str(cellValue)
         else :
             ss = str(cellValue)
     return ss
@@ -65,7 +97,8 @@ def getCell(  row       # номер строки
             else :
                 ss = str(cellValue)
         else :
-            ss = '0'
+            ss = str(float(cellValue))
+            print(cellValue, ss)
     else :
         if (cellType in (2,3)) :                    # numeric
             if int(cellValue) == cellValue:
@@ -86,6 +119,28 @@ def subInParentheses( sourceString):
     else:
         key = '' 
     return key
+
+
+
+def currencyTypeX(row, col, sheet):
+    '''
+    Функция анализирует "формат ячейки" таблицы excel, является ли он "денежным"
+    и какая валюта указана в этом формате.
+    Распознаются не все валюты и способы их описания.
+    '''
+    fmt_str = sheet.cell(row=row, column=col).number_format
+    if ('\u20bd' in fmt_str or
+        'р' in fmt_str):
+        val = 'RUR'
+    elif '\xa3' in fmt_str:
+        val = 'GBP'
+    elif chr(8364) in fmt_str:
+        val = 'EUR'
+    elif (fmt_str.find('USD')>=0) or (fmt_str.find('[$$')>=0) :
+        val = 'USD'
+    else:
+        val = ''
+    return val
 
 
 
@@ -142,3 +197,21 @@ def quoted(sss):
     if ((',' in sss) or ('"' in sss) or ('\n' in sss))  and not(sss[0]=='"' and sss[-1]=='"') :
         sss = '"'+sss.replace('"','""')+'"'
     return sss
+
+
+def nameToId(value):
+    result = ''
+    for ch in value:
+        if (ch != " " and ch != "/" and ch != "\\" and ch != '_' and ch != "," and
+                ch != "'" and ch != "!" and ch != "@" and                 # and ch != "." and ch != "-"
+                ch != "#" and ch != "$" and ch != "%" and ch != "^" and ch != "&" and
+                ch != "*" and ch != "(" and ch != ")" and ch != "[" and ch != "]" and
+                ch != "{" and ch != ":" and ch != '"' and ch != ";"):
+            result = result + ch
+
+    length = len(result)
+    if length > 50:
+        point = int(length / 2)
+        result = result[:13] + result[point - 12:point + 13] + result[-12:]
+
+    return result
--- profdisplay.cmd
+++
@@ -1,3 +0,0 @@
-python.exe profdisplay.py
-
-rem pause
